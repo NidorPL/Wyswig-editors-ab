850310ba54f65b8d6416925a877660632d0e2a43
NidorPL <david_schmidt@online.de>
2021-08-04 14:35:10 +0200

    add react-quill editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import 'froala-editor/css/froala_style.min.css';
@@ -8,10 +8,44 @@ import './App.css';
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import { Editor } from "react-draft-wysiwyg";
 import {Row, Col, Typography} from "antd"
+import { useEditor, EditorContent } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'
 
+const Tiptap = () => {
+    const editor = useEditor({
+        extensions: [
+            StarterKit,
+        ],
+        content: '<p>Hello World! 🌎️</p>',
+    })
+
+    return (
+        <EditorContent editor={editor} />
+    )
+}
 
 function App() {
-  return (
+    const [value, setValue] = useState('');
+
+    const modules = {
+            toolbar: [
+                [{ 'header': [1, 2, false] }],
+                ['bold', 'italic', 'underline','strike', 'blockquote'],
+                [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+                ['link', 'image'],
+                ['clean']
+            ],
+        }
+    const formats = [
+            'header',
+            'bold', 'italic', 'underline', 'strike', 'blockquote',
+            'list', 'bullet', 'indent',
+            'link', 'image'
+        ]
+
+    return (
     <div className="App">
 
 
@@ -36,6 +70,15 @@ function App() {
               }} />
           </Col>
       </Row>
+        <Row>
+            <Col span={15}>
+                <Typography.Title level={3} >Quill Editor</Typography.Title>
+                <ReactQuill theme="snow" value={value} onChange={setValue} modules={modules}
+                            formats={formats}/>
+
+
+            </Col>
+        </Row>
     </div>
   );
 }
